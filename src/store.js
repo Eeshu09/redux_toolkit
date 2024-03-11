52c21f865780ec2c8b2d8de676d26ccf778d60e8
Eeshu009 <eeshu@algogenix.com>
2024-03-11 17:50:45 +0530

    ---persistStore

--- src/store.js
+++ src/store.js
@@ -1,10 +1,30 @@
-import { configureStore } from "@reduxjs/toolkit";
-import cartSlice from './cartSlice'
-export const store=configureStore({
-    reducer:{ 
-        allcart:cartSlice,
+// import { configureStore } from "@reduxjs/toolkit";
+// import cartSlice from './cartSlice'
+// export const store=configureStore({
+//     reducer:{ 
+//         allcart:cartSlice,
 
 
-    } 
+//     } 
      
-}) 
+// })
+
+import { configureStore } from "@reduxjs/toolkit";
+import { persistStore, persistReducer } from 'redux-persist';
+import storage from 'redux-persist/lib/storage';
+import cartSlice from './cartSlice';
+
+const persistConfig = {
+  key: 'root',
+  storage,
+};
+
+const persistedReducer = persistReducer(persistConfig, cartSlice);
+
+export const store = configureStore({
+  reducer: {
+    allcart: persistedReducer,
+  },
+});
+
+export const persistor = persistStore(store);
